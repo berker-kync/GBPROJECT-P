bf7a76a4557cec7c8b42375b269ccc18feffbff7
00Gizem00 <gizem_fbl@windowslive.com>
2023-09-20 14:54:28 +0300

    data img silindi menü for dönüyor

--- fooddelivery/urls.py
+++ fooddelivery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= "index"),
-    path('detail-restaurant', views.detailRestaurant, name="detail-restaurant")
+    path('detail-restaurant', views.detailRestaurant, name="detail-restaurant"),
+    path('deneme', views.deneme, name="deneme"),
 ]
