9526daf92f539c0f6cf7006f0b9eb877e1faa0b4
00Gizem00 <gizem_fbl@windowslive.com>
2023-09-26 01:48:59 +0300

    js reload attim sonra html atacagm

--- fooddelivery/views.py
+++ fooddelivery/views.py
@@ -27,21 +27,6 @@ def detailRestaurant(request):
     return render(request, 'detail-restaurant.html',context)
 
 
-# def add_to_cart(request, product_id):
-#     if request.method == "POST":
-#         try:
-#             product = Product.objects.get(id=product_id)
-#             selected_quantity = int(request.POST.get('quantity', 1))  # get the quantity from POST data
-
-#             if product.quantity >= selected_quantity:
-#                 product.quantity -= selected_quantity
-#                 product.save()
-#                 return JsonResponse({"success": True, "message": "Product added to cart."})
-#             else:
-#                 return JsonResponse({"success": False, "message": "Insufficient product stock."})
-#         except (Product.DoesNotExist, ValueError):
-#             return JsonResponse({"success": False, "message": "Product not found or invalid data."})
-
 def add_to_cart(request, product_id):
     if request.method == "POST":
         try:
@@ -92,18 +77,4 @@ def remove_from_cart(request, id):
             return JsonResponse({"success": False, "message": "Product not found or invalid data."})
 
 
-
-# def cart_summary(request):
-#     # Fetch cart items for this session
-#     cart_items = ShoppingCart.objects.filter(session_key=request.session.session_key)
-
-#     # Calculate the total price
-#     total_price = sum(item.total_price for item in cart_items)
-
-#     context = {
-#         'cart_items': cart_items,
-#         'total_price': total_price,
-#     }
-    
-#     return render(request, 'order_summary.html', context)
 
