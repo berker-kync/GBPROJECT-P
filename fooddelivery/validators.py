912bb92571da8187bb3baabb73b28cde6ac39754
berker-kync <berkerkoyuncu@gmail.com>
2023-10-10 17:20:57 +0300

    Customer phone number validator minor fix

--- fooddelivery/validators.py
+++ fooddelivery/validators.py
@@ -4,5 +4,5 @@ import re
 def phone_number_validator(value):
     phone_regex = re.compile(r'^\d{10}$')
     if not phone_regex.match(value):
-        raise ValidationError('Invalid phone number.')
+        raise ValidationError('Geçersiz telefon numarası.')
     return value
