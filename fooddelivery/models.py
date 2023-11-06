86976711bdfa5efb6273932e637b75e3810520b8
00Gizem00 <gizem_fbl@windowslive.com>
2023-11-06 20:39:43 +0300

    küçük ekleme

--- fooddelivery/models.py
+++ fooddelivery/models.py
@@ -14,7 +14,6 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomerManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
