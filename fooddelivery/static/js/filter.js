f2667a147925fb11123f04c25403cf1a53be5509
00Gizem00 <gizem_fbl@windowslive.com>
2023-11-16 15:28:44 +0300

    filter js değişti

--- fooddelivery/static/js/filter.js
+++ fooddelivery/static/js/filter.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
             }
 
-            restaurantStrip.style.display = showRestaurant ? 'block' : 'none';
+            restaurantStrip.parentElement.style.display = showRestaurant ? 'block' : 'none';
         });
     });
 });
