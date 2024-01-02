d2929567fa881d569479e7c6b1cb684d235f2ad0
berker-kync <berkerkoyuncu@gmail.com>
2024-01-02 15:26:39 +0300

    setting ayarı

--- GBPROJECT/settings.py
+++ GBPROJECT/settings.py
@@ -81,15 +81,14 @@ WSGI_APPLICATION = 'GBPROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# Local development (SQLite)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': config('DATABASE_NAME', default=BASE_DIR / 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-# Heroku deployment (PostgreSQL)
 if os.environ.get('DATABASE_URL'):
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
